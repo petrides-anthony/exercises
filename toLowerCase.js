131c9cf34766d052486a8909e5ab995335250649
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2019-10-31 12:41:42 +0000

    first exercise

---
+++ toLowerCase.js
@@ -0,0 +1,18 @@
+const alphabetUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+"Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const alphabetLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+// console.log(alphabetUpperCase[1])
+
+const convertToLowerCase = function (myString) {
+    lowerCaseString= ""
+    for (let i = 0; i < myString.length; i++) {
+        for (let i = 0; i < alphabetUpperCase.length; i++) {
+
+        }
+    }
+        return lowerCaseString
+}
+
+console.log(convertToLowerCase("I AM A BIG ! STRING"))
