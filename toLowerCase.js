88f56980d7b611609a6a6659cd768e58f47fffd5
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2019-10-31 12:46:26 +0000

    attempt 2

--- toLowerCase.js
+++ toLowerCase.js
@@ -1,6 +1,8 @@
-const alphabetUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
+const alphabetUpperCase = 
+["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P",
 "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-const alphabetLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+const alphabetLowerCase = 
+["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 // console.log(alphabetUpperCase[1])
@@ -9,7 +11,10 @@ const convertToLowerCase = function (myString) {
     lowerCaseString= ""
     for (let i = 0; i < myString.length; i++) {
         for (let i = 0; i < alphabetUpperCase.length; i++) {
-
+            if (myString[i] === alphabetUpperCase[i]) {
+                lowerCaseString.concat(alphabetLowerCase[i])
+                console.log(alphabetLowerCase[i])
+            }
         }
     }
         return lowerCaseString
