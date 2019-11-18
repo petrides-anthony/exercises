e4fd2007f3d176362dd59618ed1c129ed1294c32
unknown <petrides.anthony@gmail.com>
2019-11-18 20:59:32 +0000

    started writing seachOccurences function

--- letter-searcher/letter-searcher.js
+++ letter-searcher/letter-searcher.js
@@ -2,31 +2,43 @@ const mainTextAreaElement = document.querySelector('#main-text-area')
 const singleCharacterInputElement = document.querySelector('#character-input')
 const searchButtonElement = document.querySelector('#search-characters')
 
-let searchText = undefined
-let enteredChar = undefined
+let enteredText = undefined
 let charCount = 0
+let searchChar = undefined
+let occurences = 0
+
+const searchCharOccurences = function (searchChar) {
+    for (let i = 0; i < charCount; i++) {
+        occurences = 0
+        if (searchChar === enteredText[i]) {
+            occurences = occurences + 1
+        }
+    }
+        return occurences
+}
 
 // Main Text Input Area
 mainTextAreaElement.addEventListener('input', function(e) {
-    enteredChar = e.target.value
-    charCount = e.target.value.length
+    enteredText = e.target.value
+    charCount = enteredText.length
     console.log(`Number of characters entered: ${charCount}`)
 })
 
 
 // Single Character Search Input
 singleCharacterInputElement.addEventListener('input', function(e) {
-    searchText = e.target.value
+    searchChar = e.target.value
 })
 
 // Search Characters button
 searchButtonElement.addEventListener('click', function(e) {
-    if (!searchText) {
+    if (!searchChar) {
         console.log('No Search Text Entered!')
-    } else if (searchText === " ") {
+    } else if (searchChar === " ") {
         console.log(`You are seaching for occurences of spaces.`)
     } 
     else {
-        console.log(`You are seaching for occurences of: ${searchText}.`)
+        console.log(`You are seaching for occurences of: ${searchChar}.`)
+        console.log(searchCharOccurences(searchChar))
     }
 })
