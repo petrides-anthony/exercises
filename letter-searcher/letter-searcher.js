fe382872e6b5e1e3b42e0549f8bae026c2a04588
unknown <petrides.anthony@gmail.com>
2019-11-18 20:45:47 +0000

    refactor

--- letter-searcher/letter-searcher.js
+++ letter-searcher/letter-searcher.js
@@ -2,19 +2,31 @@ const mainTextAreaElement = document.querySelector('#main-text-area')
 const singleCharacterInputElement = document.querySelector('#character-input')
 const searchButtonElement = document.querySelector('#search-characters')
 
+let searchText = undefined
+let enteredChar = undefined
+let charCount = 0
 
 // Main Text Input Area
 mainTextAreaElement.addEventListener('input', function(e) {
-    enteredText = e.target.value
-    console.log(enteredText)
+    enteredChar = e.target.value
+    charCount = e.target.value.length
+    console.log(`Number of characters entered: ${charCount}`)
 })
+
 
 // Single Character Search Input
 singleCharacterInputElement.addEventListener('input', function(e) {
-    enteredText = e.target.value
+    searchText = e.target.value
 })
 
 // Search Characters button
 searchButtonElement.addEventListener('click', function(e) {
-        console.log(`You are seaching for occurences of: ${enteredText}`)
+    if (!searchText) {
+        console.log('No Search Text Entered!')
+    } else if (searchText === " ") {
+        console.log(`You are seaching for occurences of spaces.`)
+    } 
+    else {
+        console.log(`You are seaching for occurences of: ${searchText}.`)
+    }
 })
