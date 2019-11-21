485be1e2ffa9da1e04f274ec5b9ec6f792e0bdac
Anthony Petrides <Anthony.Petrides@thetrainline.com>
2019-11-21 09:12:40 +0000

    refactor app to use arrow functions and remove needless even listeners

--- letter-searcher/letter-searcher.js
+++ letter-searcher/letter-searcher.js
@@ -2,33 +2,20 @@ const mainTextAreaElement = document.querySelector('#main-text-area')
 const singleCharacterInputElement = document.querySelector('#character-input')
 const searchButtonElement = document.querySelector('#search-characters')
 
-let enteredText = undefined
-let charCount = 0
-let searchChar = undefined
-
-const searchCharOccurences = function (searchChar) {
+const searchCharOccurences = (searchChar) => {
     let occurences = 0
-    for (let i = 0; i < enteredText.length; i++) {
-        if (searchChar === enteredText[i]) {
+    let textAreaText = document.getElementById('main-text-area').value
+    for (let i = 0; i < textAreaText.length; i++) {
+        if (searchChar === textAreaText[i]) {
             occurences++
         }
     }
         return occurences
 }
 
-// Main Text Input Area
-mainTextAreaElement.addEventListener('input', function(e) {
-    enteredText = e.target.value
-    charCount = enteredText.length
-})
-
-// Single Character Search Input
-singleCharacterInputElement.addEventListener('input', function(e) {
-    searchChar = e.target.value
-})
-
 // Search Characters button
-searchButtonElement.addEventListener('click', function(e) {
+searchButtonElement.addEventListener('click', (e) => {
+    searchChar = document.getElementById('character-input').value
     if (!searchChar) {
         document.getElementById("results").innerHTML = `<strong>No Search Text Entered...</strong>`
     } else if (searchChar === " ") {
